287bddd82b321a0529257b35be9547dcc55ae691
Joshua de Reeper <joshua@dereeper.co.nz>
2025-04-05 20:33:32 +0200

    Emulated Infinity Base (#1)
    * Infinity Backend
    
    * add infinity to UI - only allow one device to be attached at once time
    
    * ensure device exists before sending requests
    
    * call infinity base commands
    
    * add UI item for infinity figures
    
    * Add menu item and figure number changes
    
    * update descriptor
    
    * big endian sha1, remove before load

--- src/include/re_nsyshid.h
+++ src/include/re_nsyshid.h
@@ -1,11 +1,11 @@
 #pragma once
 
-#define EMULATION_STATUS_CONFIG_ID "emulationStatus"
-#define EMULATED_DEVICE_CONFIG_ID  "emulatedDevice"
+#define EMULATION_STATUS_CONFIG_ID              "emulationStatus"
+#define EMULATED_DEVICE_CONFIG_ID               "emulatedDevice"
 
-#define RE_NSYSHID_VERSION_MAJOR(v) ((v >> 16) & 0xff)
-#define RE_NSYSHID_VERSION_MINOR(v) ((v >> 8) & 0xff)
-#define RE_NSYSHID_VERSION_PATCH(v) (v & 0xff)
+#define RE_NSYSHID_VERSION_MAJOR(v)             ((v >> 16) & 0xff)
+#define RE_NSYSHID_VERSION_MINOR(v)             ((v >> 8) & 0xff)
+#define RE_NSYSHID_VERSION_PATCH(v)             (v & 0xff)
 #define RE_NSYSHID_VERSION(major, minor, patch) ((major << 16) | (minor << 8) | patch)
 
 enum EmulationStatus {
@@ -15,7 +15,8 @@ enum EmulationStatus {
 
 enum DeviceToEmulate {
     NONE      = 0,
-    SKYLANDER = 1
+    SKYLANDER = 1,
+    INFINITY  = 2
 };
 
 void ResetClientLibrary();
