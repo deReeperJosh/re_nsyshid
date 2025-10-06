52c0512c2e063704849f037e757e73c9bc83aa11
Joshua de Reeper <joshua@dereeper.co.nz>
2025-10-06 19:44:06 +0200

    Case sensitivity

--- webpage/src/main.jsx
+++ webpage/src/main.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import App from "./App";
-import Dashboard from "./Dashboard";
+import Dashboard from "./dashboard";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
